31fddcc6306e4d73dee0bfea35d7a5d25240281d
Chris Dunhill <chrisdunhill@gmail.com>
2017-11-17 16:02:52 +0000

    Early days

---
+++ workingitout.R
@@ -0,0 +1,18 @@
+
+# Plan...
+# 1. Import and clean the data - add in MW hierarchy info and description?
+# 2. Transform the data into format suitable for wordcloud
+# 3. Create wordcloud for a given T2T
+# 4. Convert to Shiny app, allowing users to select ref code (or description?)
+# 5. Publish Shiny app to users (somehow)
+
+# Some tutorials
+# Merging data frames https://rpubs.com/NateByers/Merging
+
+
+
+library (dplyr)
+
+read.csv("revs_oct17.csv")
+
+
