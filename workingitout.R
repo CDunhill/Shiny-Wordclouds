bd4cc2ad54330b1727ee3a2c8d7df52c86a66d73
Chris Dunhill <chrisdunhill@gmail.com>
2017-11-22 10:17:53 +0000

    Transformed data and added template Shiny script.

--- workingitout.R
+++ workingitout.R
@@ -1,6 +1,6 @@
 
 # Plan...
-# 1. Import and clean the data - add in MW hierarchy info and description?
+# 1. Import the data - add in MW hierarchy info and description?
 # 2. Transform the data into format suitable for wordcloud
 # 3. Create wordcloud for a given T2T
 # 4. Convert to Shiny app, allowing users to select ref code (or description?)
@@ -9,10 +9,30 @@
 # Some tutorials
 # Merging data frames https://rpubs.com/NateByers/Merging
 
+library (wordcloud)
+
+#1 Import and clean
+
+data <- read.csv("revs_oct17.csv")
+View (data)
+# Need to add in MW hierarchy (later)
 
 
-library (dplyr)
+#2 Transform by aggregating all reviews into one record per Ref
 
-read.csv("revs_oct17.csv")
+reviews <- aggregate(ReviewBody ~ Ref, data = data, paste, collapse = ",")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
